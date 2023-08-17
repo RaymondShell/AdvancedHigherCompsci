4e0173763b4c2851cf0a9410f3d3cdbdaa1942d7
Raymond <raymondshell2006@hotmail.com>
2023-08-17 21:11:36 +0100

    Added Some More Stuff

--- MySqlDatabase.py
+++ MySqlDatabase.py
@@ -9,7 +9,7 @@ try:
     if connection.is_connected():
         db_Info = connection.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
+        cursor = connection.cursor(buffered=True)
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("You're connected to database: ", record)
@@ -17,6 +17,7 @@ except Error as e:
     print("Error while connecting to MySQL", e)
 
 def closeDatabase():
+    cursor.close()
     connection.close()
     print("MySQL connection is closed")
 
@@ -28,12 +29,23 @@ def Add_Data(usr, passw):
         cursor.execute(mySql_insert_query, record)
         connection.commit()
         print(cursor.rowcount, "SignUp Successful")
-        cursor.close()
         return True
     except mysql.connector.Error as error:
+        if Error(errno=23000):
+            return "EXISTS"
         print("Failed to insert record into data table {}".format(error))
         return False
 
-#def check_Data(usr, passw):
+def check_Data(usr, passw):
+
+    print(usr, passw)
+    try:
+        cursor.execute("SELECT username, password FROM data WHERE username='%s' AND password='%s'"% (usr, passw))
+        connection.commit()
+        print(cursor.rowcount, "Login Confirmed")
+        return True
+    except mysql.connector.Error as error:
+        print("Username And Password Combo Not Found {}".format(error))
+        return False
 
 #def check_data(usr, passw):
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from tkinter import *
-from MySqlDatabase import closeDatabase, Add_Data
+from MySqlDatabase import closeDatabase, Add_Data, check_Data
 from functions import *
 window = Tk()
 window.geometry("340x440")
 signUpConfirmation = False
-loginConfirmation = False
 def unloadAll():
     Starter.pack_forget()
     gotoLogin.pack_forget()
@@ -17,6 +16,12 @@ def unloadAll():
 def prepLogin():
     window.geometry("420x231")
     unloadAll()
+    labelUsr.grid(row=1, column=0)
+    username.grid(row=1, column=1)
+    labelPswrd.grid(row=2, column=0)
+    password.grid(row=2, column=1)
+    SignIn.grid(row=3, column=1, columnspan=2)
+    window.mainloop()
 
 def prepSignup():
     window.geometry("340x440")
@@ -28,24 +33,48 @@ def prepSignup():
     SignUp.grid(row=3, column=1, columnspan=2)
     window.mainloop()
 
-def getData_AddData():
-    global signUpConfirmation
-    global loginConfirmation
+def signUp():
+    signUpConfirmation = False
     usrname = username.get()
     pssword = password.get()
-    signUpConfirmation = Add_Data(usrname, pssword)
-    if signUpConfirmation == True:
-        unloadAll()
-        prepLogin()
-    if loginConfirmation == True:
-        unloadAll()
+    if len(usrname) > 0 and len(pssword) > 0:
+        signUpConfirmation = Add_Data(usrname, pssword)
+        print(signUpConfirmation)
+        if signUpConfirmation == True:
+            unloadAll()
+            prepLogin()
+        if signUpConfirmation == "EXISTS":
+            openPopup("Username already Exists Please Try A New One")
+    else:
+        openPopup("Please Ensure the Username and password boxes are not empty")
 
+def loginCheck():
+    signInConfirmation = False
+    usrname = username.get()
+    pssword = password.get()
+    signInConfirmation = check_Data(usrname, pssword)
+    if signInConfirmation == True:
+        unloadAll()
+        prepMain()
+
+def openPopup(text):
+    top = Toplevel(window)
+    top.geometry("1000x100")
+    top.title("Child Window")
+    def exit_btn():
+        top.destroy()
+        top.update()
+    Label(top, text=text, font=('Mistral 18 bold'), anchor='center').pack()
+    Button(top, text="ok", command=exit_btn, width=10).pack()
+def prepMain():
+    print("Successful")
 
 
 Starter = Label(text="Would you like to Login Or Signup?")
 gotoLogin = Button(text="Login", command=prepLogin)
 gotoSignup = Button(text="Signup", command=prepSignup)
-SignUp = Button(window, text="Sign Up", command=getData_AddData)
+SignUp = Button(window, text="Sign Up", command=signUp)
+SignIn = Button(window, text="Sign In", command=loginCheck)
 username = Entry(window)
 password = Entry(window, show="*")
 labelUsr = Label(window, text="Username")
